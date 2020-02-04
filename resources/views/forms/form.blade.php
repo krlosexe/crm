3a78de43aecb879a0ca5e1f0ade79de09574babd
krlosexe <cardenascarlos18@gmail.com>
2020-02-04 13:53:29 -0500

    CORRCCIONES ROUTES FORM WEB

--- resources/views/forms/form.blade.php
+++ resources/views/forms/form.blade.php
@@ -12,25 +12,25 @@
   <title>App</title>
 
   <!-- Custom fonts for this template-->
-  <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?= url('/') ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="/css/sb-admin-2.min.css" rel="stylesheet">
-  <link href="/css/custom.css" rel="stylesheet">
+  <link href="<?= url('/') ?>/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?= url('/') ?>/css/custom.css" rel="stylesheet">
 
 
 
-  <link href="/vendor/bootstrap-fileinput/css/fileinput.css" media="all" rel="stylesheet" type="text/css"/>
-  <link href="/vendor/bootstrap-fileinput/themes/explorer-fas/theme.css" media="all" rel="stylesheet" type="text/css"/>
+  <link href="<?= url('/') ?>/vendor/bootstrap-fileinput/css/fileinput.css" media="all" rel="stylesheet" type="text/css"/>
+  <link href="<?= url('/') ?>/vendor/bootstrap-fileinput/themes/explorer-fas/theme.css" media="all" rel="stylesheet" type="text/css"/>
 
-  <script src="/vendor/jquery/jquery.min.js"></script>
+  <script src="<?= url('/') ?>/vendor/jquery/jquery.min.js"></script>
     <!-- Custom styles for this page -->
-  <link href="/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-  <link href="/vendor/sweetalert/sweetalert.css" rel="stylesheet">
+  <link href="<?= url('/') ?>/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="<?= url('/') ?>/vendor/sweetalert/sweetalert.css" rel="stylesheet">
 
 
-  <link href="/vendor/select2-4.0.11/dist/css/select2.min.css" rel="stylesheet" />
+  <link href="<?= url('/') ?>/vendor/select2-4.0.11/dist/css/select2.min.css" rel="stylesheet" />
 
 
 </head>
@@ -173,60 +173,59 @@
 
   <!-- Bootstrap core JavaScript-->
 
-  <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= url('/') ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?= url('/') ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="/js/sb-admin-2.min.js"></script>
+  <script src="<?= url('/') ?>/js/sb-admin-2.min.js"></script>
   
 
 
    <!-- Page level plugins -->
-  <script src="/vendor/chart.js/Chart.min.js"></script>
+  <script src="<?= url('/') ?>/vendor/chart.js/Chart.min.js"></script>
 
   <!-- Page level custom scripts -->
-  <script src="/js/demo/chart-area-demo.js"></script>
-  <script src="/js/demo/chart-pie-demo.js"></script>
+  <script src="<?= url('/') ?>/js/demo/chart-area-demo.js"></script>
+  <script src="<?= url('/') ?>/js/demo/chart-pie-demo.js"></script>
 
   
 
 
  <!-- Page level plugins -->
-    <script src="/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= url('/') ?>/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?= url('/') ?>/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="/js/demo/datatables-demo.js"></script>
+    <script src="<?= url('/') ?>/js/demo/datatables-demo.js"></script>
 
   
 
 
-    <script src="/vendor/bootstrap-fileinput/js/plugins/piexif.js" type="text/javascript"></script>
-    <script src="/vendor/bootstrap-fileinput/js/plugins/sortable.js" type="text/javascript"></script>
-    <script src="/vendor/bootstrap-fileinput/js/fileinput.js" type="text/javascript"></script>
-    <script src="/vendor/bootstrap-fileinput/js/locales/fr.js" type="text/javascript"></script>
-    <script src="/vendor/bootstrap-fileinput/js/locales/es.js" type="text/javascript"></script>
-    <script src="/vendor/bootstrap-fileinput/themes/fas/theme.js" type="text/javascript"></script>
-    <script src="/vendor/bootstrap-fileinput/themes/explorer-fas/theme.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/bootstrap-fileinput/js/plugins/piexif.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/bootstrap-fileinput/js/plugins/sortable.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/bootstrap-fileinput/js/fileinput.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/bootstrap-fileinput/js/locales/fr.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/bootstrap-fileinput/js/locales/es.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/bootstrap-fileinput/themes/fas/theme.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/bootstrap-fileinput/themes/explorer-fas/theme.js" type="text/javascript"></script>
 
 
 
 
 
-    <script src="/vendor/sweetalert/sweetalert.min.js" type="text/javascript"></script>
-    <script src="/vendor/sweetalert/sweetalert-dev.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/sweetalert/sweetalert.min.js" type="text/javascript"></script>
+    <script src="<?= url('/') ?>/vendor/sweetalert/sweetalert-dev.js" type="text/javascript"></script>
 
-    <script src="/vendor/select2-4.0.11/dist/js/select2.min.js"></script>
+    <script src="<?= url('/') ?>/vendor/select2-4.0.11/dist/js/select2.min.js"></script>
 
 
-  <script src="/js/funciones.js"></script>
+  <script src="<?= url('/') ?>/js/funciones.js"></script>
   
 
   <script>
     var user_id = localStorage.getItem('user_id');
-	$("#logout").attr("href", "logout/"+user_id)
 	
 
 	$(document).ready(function(){
