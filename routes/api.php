508e57c1c935612cd0ef3689ceb9ced3b85a51c0
krlosexe <cardenascarlos18@gmail.com>
2020-02-10 10:04:57 -0500

    EMAIL

--- routes/api.php
+++ routes/api.php
@@ -114,5 +114,4 @@ Route::get('notifications/generate', 'NotificationsController@Generate');
 
 
 
-Route::get('email/forms', 'NotificationsController@Email');
 Route::post('email/forms', 'NotificationsController@Email');
