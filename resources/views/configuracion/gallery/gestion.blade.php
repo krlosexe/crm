a9650a83aaf20d0c308cd84480b66adb10338834
Carlos Cardenas <carloscardenas@Carloss-iMac.local>
2020-05-13 13:51:25 -0500

    GALLERY IMAGE

--- resources/views/configuracion/gallery/gestion.blade.php
+++ resources/views/configuracion/gallery/gestion.blade.php
@@ -48,7 +48,7 @@
 			        <div class="container-fluid">
 
 			          <!-- Page Heading -->
-			          <h1 class="h3 mb-2 text-gray-800">Ciudades</h1>
+			          <h1 class="h3 mb-2 text-gray-800">Galeria</h1>
 
 			          <div id="alertas"></div>
 			          <input type="hidden" class="id_user">
