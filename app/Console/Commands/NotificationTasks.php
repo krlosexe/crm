de30e5f84200c08c3e63f28d41f62db969702a4f
Carlos Cardenas <carloscardenas@Carloss-iMac.local>
2020-05-15 19:30:04 -0500

    CORERCCION COMMAND TASKS

--- app/Console/Commands/NotificationTasks.php
+++ app/Console/Commands/NotificationTasks.php
@@ -93,8 +93,8 @@ class NotificationTasks extends Command
         $user = User::find($data["user_id"]);
         $subject = $data["issue"];
 
-        $for = "cardenascarlos18@gmail.com";
-        //$for = $user["email"];
+        //$for = "cardenascarlos18@gmail.com";
+        $for = $user["email"];
 
         $request["msg"] = $data["mensage"];
 
