91cab012c107e8793fa706e997f4930c5804fb5a
Carlos Cardenas <carloscardenas@Carloss-iMac.local>
2020-05-20 16:22:26 -0500

    CORS EXPORT XLS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,10 +15,8 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-
-        print_r($request["REDIRECT_URL"]);
         return $next($request)
-               // ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
     }
