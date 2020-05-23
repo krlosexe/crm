2939d3db072d8b71315869cc0917a59873e0f705
Carlos Cardenas <carloscardenas@Carloss-iMac.local>
2020-05-22 21:08:37 -0500

    access-token header cors

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -502,8 +502,14 @@ class ClientsController extends Controller
                             ->where("clientes.code_client", $code)
                             
                             ->first();
+
+        if($data){
+            return response()->json($data)->setStatusCode(200);
+        }else{
+            return response()->json([])->setStatusCode(400);
+        }
         
-        return response()->json($data)->setStatusCode(200);
+        
        
     }
 
