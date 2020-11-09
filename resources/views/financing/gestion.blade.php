6127224d608ca3001bbb6782fbbde9dd911b5541
Gil <dev.javiergil@gmail.com>
2020-11-09 09:35:11 -0500

    ajustes

--- resources/views/financing/gestion.blade.php
+++ resources/views/financing/gestion.blade.php
@@ -367,8 +367,8 @@
 		$(select).click(function(e) {
 			var url = document.getElementById('ruta').value;
 			$.ajax({
-				// url: 'https://pdtclientsolutions.com/crm-public/api/client/indentification/' + $("#indetification").val(),
-				url: url + '/api/client/indentification/' + $("#indetification").val(),
+				url: 'https://pdtclientsolutions.com/crm-public/api/client/indentification/' + $("#indetification").val(),
+				//url: url + '/api/client/indentification/' + $("#indetification").val(),
 				type: 'GET',
 				dataType: 'JSON',
 				async: false,
