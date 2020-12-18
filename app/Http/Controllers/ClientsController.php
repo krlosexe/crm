e883de12a6692cf4dd4fba4cd99d54f893e19313
javier gil <dev.javiergil@gmail.com>
2020-12-18 09:42:00 -0500

    cambios

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -2201,7 +2201,7 @@ class ClientsController extends Controller
                     $for = $users["email"];
                 // $for = "cardenascarlos18@gmail.com";
 
-                    $request["msg"]  = "Un Paciente se ha registrado por el App";
+                    $request["msg"]  = "Un Paciente se ha registrado por el App con el codigo ".$request["code_adviser"];
                     $request["apellidos"]        = ".";
                     $request["direccion"]        = ".";
                     $request["fecha_nacimiento"] = date("Y-m-d");
