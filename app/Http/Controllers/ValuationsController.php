29d33026326e2ac9214efccfb6dae0f97139c6e8
javier gil <dev.javiergil@gmail.com>
2021-02-01 11:13:49 -0500

    CAMBIOS

--- app/Http/Controllers/ValuationsController.php
+++ app/Http/Controllers/ValuationsController.php
@@ -819,6 +819,7 @@ class ValuationsController extends Controller
                             ->where("valuations.status", 1)
                             ->where("auditoria.usr_regins", $user_id)
                             ->whereRaw("month(fecha) = ".date("m")." ")
+                            ->whereRaw("year(fecha) = ".date("Y")." ")
                             ->first();
 
         return response()->json($data)->setStatusCode(200);
